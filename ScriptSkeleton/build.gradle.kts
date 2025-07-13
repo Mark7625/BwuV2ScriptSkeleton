1e629b4f6e9bdbe665a9d65b7b60073d4a2931dd
AndNowWhat <malisaleez@gmail.com>
2025-07-13 23:44:27 +0200

    Clean dependencies

--- ScriptSkeleton/build.gradle.kts
+++ ScriptSkeleton/build.gradle.kts
@@ -4,8 +4,3 @@ plugins {
     kotlin("jvm")
     java
 }
-
-dependencies {
-    implementation(files("C:\\Users\\eztop\\.BotWithUs\\BotWithUsScriptsV2\\api-1.0.0-SNAPSHOT.jar"))
-    implementation(files("C:\\Users\\eztop\\.BotWithUs\\BotWithUsScriptsV2\\imgui-1.0.0-SNAPSHOT.jar"))
-}

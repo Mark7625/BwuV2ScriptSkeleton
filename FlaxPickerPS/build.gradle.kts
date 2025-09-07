79824b1da13fbd87b6d676600f9cbf2d4ce3bd98
Proxify <5466312+Proxify@users.noreply.github.com>
2025-09-07 04:12:51 -0500

    Updated to v2.0 xapi

--- FlaxPickerPS/build.gradle.kts
+++ FlaxPickerPS/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     implementation("net.botwithus.api:api:1.0.+")
     implementation("net.botwithus.imgui:imgui:1.0.+")
 
-    implementation("net.botwithus.xapi:xapi:1.0.+")
-    includeInJar("net.botwithus.xapi:xapi:1.0.+")
+    implementation("net.botwithus.xapi:xapi:2.0.+")
+    includeInJar("net.botwithus.xapi:xapi:2.0.+")
 }
